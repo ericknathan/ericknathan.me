527365086d3ab00549c4d5d37a869f5a5f418461
Erick Nathan <erick.capito@pagtel.com.br>
2023-11-03 13:02:44 -0300

    feat(app/setup): add under construction component to setup page

---
+++ src/app/setup/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+import { UnderConstructionPage } from "../under-construction";
+
+export const metadata: Metadata = {
+  title: "My setup",
+};
+
+export default function SetupPage() {
+  return <UnderConstructionPage />;
+}
