7ca2155ac22d4c7fad32fa17e5789da9f0d3df61
ericknathan <erick.capito@hotmail.com>
2023-12-29 22:47:05 -0300

    fix: firebase "Component auth has not been registered yet" error

--- src/lib/firebase/app.ts
+++ src/lib/firebase/app.ts
@@ -1,28 +1,15 @@
 import { initializeApp } from "firebase/app";
 
-const firebaseAppSingleton = () => {
-  const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com`,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.appspot.com`,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  };
-
-  return initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-type FirebaseAppSingleton = ReturnType<typeof firebaseAppSingleton>;
-
-const globalForFirebaseApp = globalThis as unknown as {
-  firebaseApp: FirebaseAppSingleton | undefined;
-};
-
-const firebaseApp = globalForFirebaseApp.firebaseApp ?? firebaseAppSingleton();
+const firebaseApp = initializeApp(firebaseConfig);
 
 export default firebaseApp;
-
-if (process.env.NODE_ENV !== "production")
-  globalForFirebaseApp.firebaseApp = firebaseApp;
